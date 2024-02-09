1f10a090eec95b41e5511026416ecd6525b2f107
Tristan Orndoff <61711706+BreakingDusk397@users.noreply.github.com>
2024-02-09 12:59:19 -0600

    Update nEWEST1.py

--- nEWEST1.py
+++ nEWEST1.py
@@ -885,7 +885,7 @@ def create_features(dataset):
         dataset['last_return'] = np.log(dataset["open"]).pct_change()
         dataset['last_return_5mu'] = dataset['last_return'].rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,})
         dataset['last_return_5_sum'] = dataset['last_return'].rolling(5).sum(engine='numba', engine_kwargs={"nogil":True, "nopython": True,})
-        dataset['last_return_10mu'] = dataset['last_return'].rolling(10).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,})
+        dataset['last_return_10mu'] = dataset['last_re turn'].rolling(10).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,})
         dataset['last_return_10_sum'] = dataset['last_return'].rolling(10).sum(engine='numba', engine_kwargs={"nogil":True, "nopython": True,})
 
         dataset['std_normalized'] = np.log(dataset[column_price]).rolling(std_period).apply(std_normalized, engine='numba', raw=True, engine_kwargs={"nogil":True, "nopython": True,})
@@ -913,7 +913,7 @@ def create_features(dataset):
         
         dataset['variance'] = (np.log(dataset['open']).rolling(5).std(engine='numba', engine_kwargs={"nogil":True, "nopython": True,}) * np.sqrt(5)).rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,})
         dataset['open+var'] = np.log(dataset['open']).rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,}) + (np.log(dataset['open']).rolling(5).std(engine='numba', engine_kwargs={"nogil":True, "nopython": True,}) * np.sqrt(5)).rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,})
-        dataset['open-var'] = np.log(dataset['open']).rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,}) - (np.log(dataset['open']).rolling(5).std(engine='numba', engine_kwargs={"nogil":True, "nopython": True,}) * np.sqrt(5)).rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,})
+        dataset['open_var'] = np.log(dataset['open']).rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,}) - (np.log(dataset['open']).rolling(5).std(engine='numba', engine_kwargs={"nogil":True, "nopython": True,}) * np.sqrt(5)).rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,})
         dataset['open+var_diff'] = np.cumsum(np.log(dataset['open']).rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,}).pct_change() - dataset['open+var'].shift(1))
         dataset['open+var_diff'] = np.cumsum(np.log(dataset['open']).rolling(5).mean(engine='numba', engine_kwargs={"nogil":True, "nopython": True,}).pct_change() - dataset['open-var'].shift(1))
         
